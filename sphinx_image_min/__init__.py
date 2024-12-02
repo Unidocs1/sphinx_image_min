571c34fb798121ef2f1f4b89c7bdc045bc0dd6c9
Dylan Hunt <d.hunt@xsolla.com>
2024-12-02 11:38:43 +0800

    fix: Corrected meta ver import

--- sphinx_image_min/__init__.py
+++ sphinx_image_min/__init__.py
@@ -5,7 +5,7 @@ import importlib.metadata
 
 # Dynamically fetch the version from pyproject.toml
 try:
-    __version__ = importlib.metadata.version("sphinx_openapi")
+    __version__ = importlib.metadata.version("sphinx_image_min")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0"
 
@@ -13,7 +13,7 @@ except importlib.metadata.PackageNotFoundError:
 # ENTRY POINT >>
 def setup(app: Sphinx):
     # Configuration values for the extension from that project's conf.py
-    print(f"[sphinx_openapi::setup] Extension loaded with version: {__version__}")
+    print(f"[sphinx_image_min::setup] Extension loaded with version: {__version__}")
     app.add_config_value("img_optimization_enabled", True, "env", [bool])
     app.add_config_value("img_optimization_max_width", 1920, "env", [int])
 
